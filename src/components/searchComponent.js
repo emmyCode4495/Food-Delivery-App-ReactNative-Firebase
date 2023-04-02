53216fe0b1073230cc9ec265a4fc2eeb89df081e
emmyCode4495 <emmycode4495@gmail.com>
2023-04-02 22:17:25 +0100

    Finished the first section of the search page

---
+++ src/components/searchComponent.js
@@ -0,0 +1,199 @@
+import React, {useRef,useState} from 'react'
+
+import {useNavigation} from '@react-navigation/native'
+
+import {View, Text, StyleSheet, 
+        TouchableWithoutFeedback,
+        Modal, TextInput,FlatList, 
+        TouchableOpacity,
+         Keyboard} from 'react-native'
+
+import * as Animatable from 'react-native-animatable'
+
+import {colors} from '../global/styles'
+
+import {Icon} from 'react-native-elements'
+import { filterData } from '../global/data'
+import filter from 'lodash/filter'
+
+export default function SearchScreenComponent(){
+
+    const navigation = useNavigation()
+    const [data, setData] = useState([...filterData])
+    const [modalVisible, setModalVisible] = useState(false)
+    const [textInputFocused, setTextInputFocused] = useState(true)
+
+    const textInput = useRef(0)
+    const contains = ({name},query)=>{
+        if(name.includes(query)){
+            return true
+        }
+        return false
+    }
+
+    const handleSearch =text =>{
+        const dataS = filter(filterData,user=>{
+            return contains(user,text)
+        })
+        setData([...dataS])
+    }
+    return(
+        <View style={{alignItems:'center'}}>
+        <TouchableWithoutFeedback
+            onPress={()=>{
+                setModalVisible(true)
+            }}
+        >
+            <View style={styles.searchArea}>
+                <Icon 
+                    type='material'
+                    name='search'
+                    style={styles.searchIcon}
+                    iconStyle = {{marginLeft:5}}
+                    size={20}
+                />
+                <Text style={{fontSize:20, marginLeft:25}}>Search</Text>
+            </View>
+        </TouchableWithoutFeedback>
+
+        <Modal
+        animationType='fade'
+        transparent={false}
+        visible={modalVisible}
+        >
+            <View style={styles.modal}>
+                <View style={styles.view2}>
+                    <View style={styles.textInput}>
+                        <Animatable.View
+                            animation={textInputFocused? "fadeInRight" : "fadeInLeft"}
+                            duration = {400}
+                        >
+                            <Icon
+                                name={textInputFocused ? "arrow-back" : "search"}
+                                onPress={()=>{
+                                    if(textInputFocused)
+                                    setModalVisible(false)
+                                    setTextInputFocused(true)
+                                }}
+                                style={styles.icon2}
+                                type="material"
+                                iconStyle={{marginRight:5}}
+                            />
+                        </Animatable.View>
+                        <TextInput 
+                            style={styles.textInputStyle}
+                            placeholder = ""
+                            autoFocus={false}
+                            ref = {textInput}
+
+                            onFocus={()=>{
+                                setTextInputFocused(true)
+                            }}
+                            onBlur={()=>{
+                                setTextInputFocused(false)
+                            }}
+                            onChangeText={handleSearch}
+                        />
+
+                        <Animatable.View
+                            animation={textInputFocused? "fadeInLeft" : "fadeInRight"}
+                            duration = {400}
+                        >
+                            <Icon name={textInputFocused ? 'cancel': null}
+                                iconStyle={{color:colors.grey3}}
+                                type='material'
+                                style={{marginRight: -10}}
+                                onPress={()=>{
+                                  textInput.current.clear()  
+                                  //handleSearch()
+                                }}
+                            />
+                        </Animatable.View>
+                    </View>
+                </View>
+
+                <FlatList
+                    data={data}
+                    renderItem={({item})=>(
+                        <TouchableOpacity
+                            onPress={()=>{
+                                Keyboard.dismiss
+                                navigation.navigate('searchScreen',{item:item.name})
+                                setModalVisible(false)
+                                setTextInputFocused(true)
+                            }}>
+                                <View style={styles.view2}>
+                                    <Text style={{color:colors.grey2,fontSize:15}}>{item.name}</Text>
+                                </View>
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={item=>item.id}
+                />
+            </View>
+
+        </Modal>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    text1:{
+
+    },
+    textInput:{
+        borderWidth:1,
+        marginHorizontal:0,
+        borderRadius:12,
+        borderColor:"#86939e",
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        alignContent:'center',
+        alignItems:'center',
+        paddingLeft:10,
+        paddingRight:10
+    },
+    searchArea:{
+        marginTop:10,
+        width:"94%",
+        height:50,
+        backgroundColor:colors.grey5,
+        borderRadius:15,
+        borderWidth:1,
+        borderColor:colors.grey4,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    searchIcon:{
+        fontSize:24,
+        padding:5,
+        color:colors.grey2
+    },
+    view1:{
+        height:20,
+        justifyContent:'center',
+        paddingHorizontal:10
+    },
+    view2:{
+        flexDirection:'row',
+        padding:15,
+        alignItems:'center'
+    },
+    icon2:{
+        fontSize:24,
+        padding:5,
+        color:colors.grey2
+    },
+    modal:{
+        flex:1
+    },
+    textInputStyle:{
+        width:"94%",
+        height:50,
+        borderRadius:20
+    }
+})
--- src/screens/contentScreens/searchScreenTab.js
+++ src/screens/contentScreens/searchScreenTab.js
@@ -1,11 +1,123 @@
 import React from 'react'
 
-import {View, Text, StyleSheet} from 'react-native'
+import {View, 
+        Text, 
+        StyleSheet, 
+        FlatList, 
+        TouchableWithoutFeedback, 
+        ImageBackground,
+         Dimensions} from 'react-native'
+
+import SearchScreenComponent from '../../components/searchComponent'
+import { filterData } from '../../global/data'
+import { colors } from '../../global/styles'
+
+
+const SCREENWIDTH = Dimensions.get('window').width;
 
 export default function SearchScreen(){
     return(
-        <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-        <Text>Search Screen</Text>
+        <View style={{flex:1,marginBottom:20}}>
+        <SearchScreenComponent/>
+        
+        <View style={{marginTop:10}}>
+        <View>
+            <FlatList 
+                data = {filterData}
+                keyExtractor = {item=>item.id}
+                renderItem = {({item})=>(
+                   <TouchableWithoutFeedback>
+                        <View style={styles.imageView}>
+                            <ImageBackground
+                                style={styles.imageStyle}
+                                source={item.image}
+                            >
+                                <View style={styles.textView}>
+                                    <Text style={{color:colors.cardBackground}}>
+                                         {item.name}
+                                    </Text>
+                                </View>
+                                
+                            </ImageBackground>
+                        </View>
+                   </TouchableWithoutFeedback> 
+                )}
+                horizontal= {false}
+                showsVerticalScrollIndicator={false}
+                numColumns = {2}
+                ListHeaderComponent = {<Text style={styles.ListHeader}>Top Categories</Text>}
+                ListFooterComponent = {<Footer />}
+            />
         </View>
+
+        </View>
+
+    </View>
     )
 }
+
+const Footer =()=>{
+ return(
+    <View style={{marginTop:10}}>
+        <View style ={{marginBottom:20}}>
+            <FlatList 
+                data = {filterData}
+                keyExtractor = {item=>item.id}
+                renderItem = {({item})=>(
+                   <TouchableWithoutFeedback>
+                        <View style={styles.imageView}>
+                            <ImageBackground
+                                style={styles.imageStyle}
+                                source={item.image}
+                            >
+                                <View style={styles.textView}>
+                                    <Text style={{color:colors.cardBackground}}>
+                                         {item.name}
+                                    </Text>
+                                </View>
+                                
+                            </ImageBackground>
+                        </View>
+                   </TouchableWithoutFeedback> 
+                )}
+                horizontal= {false}
+                showsVerticalScrollIndicator={false}
+                numColumns = {2}
+                ListHeaderComponent = {<Text style={styles.ListHeader}>More Categories</Text>}
+               
+            />
+        </View>
+
+        </View>
+ )
+}
+
+const styles = StyleSheet.create({
+    imageView:{
+        borderRadius:20,
+        justifyContent:'center',
+        alignItems:'center',
+        width:SCREENWIDTH*0.4475,
+        height: SCREENWIDTH*0.4475,
+        marginLeft: SCREENWIDTH*0.035,
+        marginBottom: SCREENWIDTH*0.035,
+    },
+    imageStyle:{
+        width:SCREENWIDTH*0.4475,
+        height: SCREENWIDTH*0.4475,
+        borderRadius:15
+    },
+    ListHeader:{
+        fontSize:16,
+        color: colors.grey2,
+        paddingBottom: 30,
+        marginLeft: 12
+    },
+    textView:{
+        width:SCREENWIDTH*0.4475,
+        height: SCREENWIDTH*0.4475,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'rgba(52,52,52,0.3)'
+    }
+})
